d82417d9178dba61e635f454e39fb3eabcc1d6c2
Hello1102 <155940545+Hello1102@users.noreply.github.com>
2024-05-25 18:34:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "5400")
 )
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002198978778"))
+LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
 # Get this value from @BEWAFAMUSICBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "6713421664"))
