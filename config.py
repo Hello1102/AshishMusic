5c3718d892250f5f09020e02acc9bae8a7fddac7
Hello1102 <155940545+Hello1102@users.noreply.github.com>
2024-05-25 23:14:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BWFTIME/AshishMusic",
+    "https://github.com/BWFTIME/sanatanixmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bwf")
 GIT_TOKEN = getenv(
